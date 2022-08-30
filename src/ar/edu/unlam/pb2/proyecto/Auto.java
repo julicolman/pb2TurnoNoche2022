4283e0eec1d2de797fb5b0997b0734c3876d61d3
julicolman <jucolman@alumno.unlam.edu.ar>
2022-08-30 20:10:03 -0300

    cambio finalizado

--- src/ar/edu/unlam/pb2/proyecto/Auto.java
+++ src/ar/edu/unlam/pb2/proyecto/Auto.java
@@ -2,20 +2,16 @@ package ar.edu.unlam.pb2.proyecto;
 
 public class Auto {
 
-<<<<<<< HEAD
+
 	private String modelo;
 	private Integer patente;
 	
-	public Auto(String modelo, Integer patente) {
+	
+	public Auto (String modelo, Integer patente) {
 		super();
 		this.modelo = modelo;
 		this.patente = patente;
-=======
-	private String patete;
-	
-	public Auto (String patente) {
-		super();
-		this.patete = patente;
->>>>>>> otrarama
+		
+
 	}
 }
