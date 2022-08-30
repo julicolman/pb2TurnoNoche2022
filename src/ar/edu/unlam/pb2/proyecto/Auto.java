95176477b1468b8425bb8ed672a36a99a016a37e
julicolman <jucolman@alumno.unlam.edu.ar>
2022-08-30 20:05:36 -0300

    agrego patente y modelo

---
+++ src/ar/edu/unlam/pb2/proyecto/Auto.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.pb2.proyecto;
+
+public class Auto {
+
+	private String modelo;
+	private Integer patente;
+	
+	public Auto(String modelo, Integer patente) {
+		super();
+		this.modelo = modelo;
+		this.patente = patente;
+	}
+}
